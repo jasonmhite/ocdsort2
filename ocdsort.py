e9918f887d17861997584677cd99abb0f88909dd
Jason Hite <jasonmhite@gmail.com>
2015-02-12 19:34:35 -0500

    Initial

---
+++ ocdsort.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python3
+import yaml
+import click
+import os
+import tvnamer
+import shutil
+from tvnamer import utils
+from fuzzywuzzy import process
+
+EPSILON = 80
+
+@click.group()
+def main():
+    pass
+
+def parse_config(filename):
+    with open(filename, 'r') as f:
+        config = yaml.load(f.read())
+
+    return config['config'], config['shows']
+
+def build_index(shows):
+    aliases = {}
+    # Invert the dictionary
+    for key, value in shows.items():
+        aliases[key] = key
+        for alias in value['names']:
+            aliases[alias] = key
+
+    return aliases
+
+config, shows = parse_config('/home/jmhite/Dev/ocdsort3/config.yml')
+all_shows = build_index(shows)
+
+@click.command()
+@click.argument('path', type=click.Path(exists=True))
+@click.option('--dry', is_flag=True)
+def sort(path, dry):
+    filenames = utils.FileFinder(
+        path,
+        with_extension=config['valid_extensions'],
+        recursive=True,
+    ).findFiles()
+
+    results = generate_names(identify(parse(filenames)))
+
+    success = list(filter(lambda s: not s['failed'], results))
+    fail = list(filter(lambda s: s['failed'], results))
+
+
+    print_results(success, fail)
+
+    if not dry:
+        click.secho("\n")
+        click.confirm("Proceed to move files?", abort=True)
+
+        with click.progressbar(success) as bar:
+            move_files(success)
+
+def parse(filenames):
+    for filename in filenames:
+        try:
+            r = utils.FileParser(filename).parse()
+            info = r.getepdata()
+
+            info['failed'] = False
+            info['failure_reason'] = None
+
+            if info['episode'] is None:
+                raise KeyError("Could not parse episode number")
+
+            elif info['seriesname'] is None:
+                raise KeyError("Could not parse series name")
+
+            else:
+                info['filename'] = filename
+                yield info
+
+        except (tvnamer.tvnamer_exceptions.InvalidFilename, KeyError) as e:
+            # This type of error is a bit different, so this file
+            # will be ignored and won't be reported.
+            click.secho(
+                "Error processing {} -> {}".format(
+                        os.path.basename(filename),
+                        e
+                    )
+            )
+            continue
+
+def identify(episodes):
+    for info in episodes:
+        if info['failed']:
+            continue
+
+        alias_name, confidence = process.extractOne(
+            info['seriesname'],
+            all_shows.keys(),
+        )
+
+        if confidence > config['threshold']:
+            info["identified_as"] = all_shows[alias_name]
+            info["confidence"] = confidence
+
+        else:
+            info['failed'] = True
+            info['failure_reason'] = "Series not identified (confidence={})".format(confidence)
+
+        yield info
+
+def generate_names(episodes):
+    for info in episodes:
+        if info['failed']:
+            continue
+
+        try:
+            title = info['identified_as']
+            extension = info['ext']
+
+            # Check for optional overrides
+            try:
+                info['offset'] = shows[title]['episode_offset']
+                episode = int(info['episode']) + info['offset']
+            except KeyError:
+                info['offset'] = 0
+                episode = int(info['episode'])
+
+            try:
+                info['season'] = int(shows[title]['season'])
+            except KeyError:
+                info['season'] = 1
+
+            info["new_name"] = utils.makeValidFilename(
+                "{identified_as} - S{season}E{episode}{ext}".format(**info),
+            )
+
+        except Exception as e:
+            info['failed'] = True
+            info['failure_reason'] = "Error during name generation ({})".format(e)
+
+        yield info
+
+def move_files(episodes, clean=True):
+    for episode in episodes:
+        if info['failed']:
+            continue
+
+        try:
+            # Note: don't think the renamer in tvnamer works correctly
+
+            new_path = os.path.join(
+                config['destination'],
+                info['identified_as'],
+            )
+
+            # See if directory exists, otherwise make it
+            try:
+                os.makedirs(new_path)
+            except OSError as e:
+                if e.errno != 17:
+                    raise
+
+            new_full_name = os.path.join(new_path, episode['new_name'])
+
+            shutil.copyfile(
+                episode['filename'],
+                new_full_name,
+            )
+
+            if clean:
+                os.unlink(episode['filename'])
+
+            info['moved_to'] = new_full_name
+
+        except Exception as e:
+            info['failed'] = True
+            info['failure_reason'] = "Error moving file ({})".format(e)
+
+def print_results(success, fail):
+    click.secho("Successfully identified:")
+    for item in success:
+        click.secho("    {filename} -> {new_name}".format(**item))
+
+    if len(fail) > 0:
+        click.secho("\n")
+        click.secho("Failures:")
+        for item in fail:
+            click.secho("    {filename} -> {failure_reason}".format(**item))
+
+if __name__ == '__main__':
+    sort()
